533d030b379535bdd7b410412d7ec4b0a04bcd16
Lashapurtseladze <149567152+Furcell@users.noreply.github.com>
2024-08-20 20:37:57 +0400

    add TextInput.tsx

---
+++ app/components/TextInput/TextInput.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export default () => {
+  const [text, setText] = useState("Misha");
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <>
+      {text && <h1>სალამი {text}!</h1>}
+
+      <input
+        type="text"
+        value={text}
+        onChange={onChange}
+        placeholder="ჩაწერე შენი სახელი"
+      />
+    </>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,17 @@
 import styles from "./page.module.css";
 import Button from "./components/Button/Button";
 import Counter from "./components/Counter/Counter";
+import TextInput from "./components/TextInput/TextInput";
 
 export default function Home() {
   const userLoggedIn = 0;
   return (
     <main className={styles.main}>
       <div className={styles.center}>
-        <Counter/>
+        <Counter />
+        <TextInput />
         {!!userLoggedIn ? (
-          <Button  title="გამოსვლა" />
+          <Button title="გამოსვლა" />
         ) : (
           <Button title="რეგისტრაცია" />
         )}
