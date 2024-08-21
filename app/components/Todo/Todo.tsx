a653cf6262c1fc7bcfe1e28ec6f8955f99063648
Lashapurtseladze <149567152+Furcell@users.noreply.github.com>
2024-08-21 23:55:38 +0400

    2

--- app/components/Todo/Todo.tsx
+++ app/components/Todo/Todo.tsx
@@ -2,15 +2,24 @@ import { useState } from "react";
 
 export default () => {
   const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState(["task1", "task2"]);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTask(e.target.value);
   };
 
+  const onAdd = () =>{
+    setTasks([...tasks,task])
+    setTask(" ");
+  }
+
   return (
     <div>
       <input type="text" value={task} onChange={onChange} />
-      <button>დამატება</button>
+      <button onClick={onAdd}>დამატება</button>
+      <ul>
+        {tasks.map(task => <li>{task}</li>)}
+      </ul>
     </div>
   );
 };
