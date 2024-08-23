f2c9f50c27d5e428c14b37c786cc1526a2ccd2f8
Lashapurtseladze <149567152+Furcell@users.noreply.github.com>
2024-08-24 01:16:37 +0400

    1

---
+++ app/loading.tsx
@@ -0,0 +1,3 @@
+export default ()=>{
+    return (<div><h1 style={{color:"red"}}>Page is Loading</h1></div>)
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-
 import styles from "./page.module.css";
 
 // Components
@@ -11,15 +10,30 @@ import CheckBox from "./components/CheckBox/CheckBox";
 import Todo from "./components/Todo/Todo";
 import Products from "./components/Products/Products";
 import Filter from "./components/Filters/Filter";
+import NotFound from "./not-found";
+import { AwaitedReactNode} from "react";
+
+
+const newPromise = new Promise<AwaitedReactNode>((res,rej)=>{
+  setTimeout(() => {
+    res("Yay!")
+  }, 3000);
+})
 
 export default function Home() {
+
+  const promise = newPromise;
+
   // const userLoggedIn = 0;
+
   return (
     <main className={styles.main}>
       <div className={styles.center}>
+        <NotFound/>
+        {promise}
         <Filter />
         <Products />
-        
+
         {/* <Todo />
         <Counter />
         <TextInput />
